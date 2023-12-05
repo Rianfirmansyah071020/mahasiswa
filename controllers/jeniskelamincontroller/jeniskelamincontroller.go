c4c5322be153a9ddb11de4be1de5c2aa01326bf0
Rianfirmansyah071020 <rian071020@gmail.com>
2023-12-05 11:29:15 +0700

    perbaikan navbar

---
+++ controllers/jeniskelamincontroller/jeniskelamincontroller.go
@@ -0,0 +1,18 @@
+package jeniskelamincontroller
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	temp, err := template.ParseFiles("views/pages/jeniskelamin/index.html")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	
+	temp.Execute(w, nil)
+}
--- routes/web/web.go
+++ routes/web/web.go
@@ -2,9 +2,11 @@ package web
 
 import (
 	"mahasiswa/controllers/homecontroller"
+	"mahasiswa/controllers/jeniskelamincontroller"
 	"net/http"
 )
 
 func Route() {
 	http.HandleFunc("/", homecontroller.Index)
+	http.HandleFunc("/jeniskelamin", jeniskelamincontroller.Index)
 }
